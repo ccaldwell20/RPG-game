f53a1f7c5c6f6275ff4d56aa7e93f379947f1083
Christian Caldwell <ccaldwell20@sciototech.org"git config --global user.email ccaldwell20@sciototech.orggit config --global user.email ccaldwell20@sciototech.org>
2019-08-27 14:08:21 -0400

    WSAD MOVEMENT BOI!!!!

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ var R = 1; //RADIUS OF PLAYER TEST CIRCLE
 var colortest = '#FF0000'
 var playerX = 20;
 var playerY = 20;
-
 var directionRight;
 var directionLeft;
 var directionUp;
@@ -13,12 +12,14 @@ var directionUpRight;
 var directionDownLeft;
 var directionDownRight;
 
-var movementSpeed = 0.5;
 
-var codeKeyRight = 68;
-var codeKeyLeft = 65;
+
 var codeKeyUp = 87;
 var codeKeyDown = 83;
+var codeKeyLeft = 65;
+var codeKeyRight = 68;
+
+var movementSpeed = 0.5;
 
 function drawPlayer() {
     var c = document.getElementById("myCanvas");
@@ -40,21 +41,21 @@ function onkeydown(e) {
         directionRight = true;
         
     } //right arrow
-    
-    else if (e.keyCode == codeKeyLeft) {
+    if (e.keyCode == codeKeyLeft) {
         directionLeft = true;
         
     } //left arrow
     
-    else if (e.keyCode == codeKeyUp) {
+    if (e.keyCode == codeKeyUp) {
         directionUp = true;
         
     } //up arrow
     
-    else if (e.keyCode == codeKeyDown) {
+    if (e.keyCode == codeKeyDown) {
         directionDown = true;
         
     } //down arrow
+    
     
 }
 
@@ -63,19 +64,18 @@ function onkeyup(e) {
         directionRight = false;
         
     } //right arrow
-    else if (e.keyCode == codeKeyLeft) {
+    if (e.keyCode == codeKeyLeft) {
         directionLeft = false;
         
     } //left arrow
-    else if (e.keyCode == codeKeyUp) {
+    if (e.keyCode == codeKeyUp) {
         directionUp = false;
         
     } //up arrow
-    else if (e.keyCode == codeKeyDown) {
+    if (e.keyCode == codeKeyDown) {
         directionDown = false;
         
     } //down arrow
-    
     
 }
 
@@ -85,17 +85,18 @@ function playerMovement() {
         playerX+= movementSpeed;
     } 
     
-    else if (directionLeft == true) {
+    if (directionLeft == true) {
         playerX-= movementSpeed;
     } 
     
-    else if (directionUp == true) {
+    if (directionUp == true) {
         playerY-= movementSpeed;
     } 
     
-    else if (directionDown == true) {
+    if (directionDown == true) {
         playerY+= movementSpeed;
     } 
+    
 }
 
 
